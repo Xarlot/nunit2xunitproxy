4bc90c20d5fbb56b79c494b62f6ea68be9d6e29a
Maxim Litvinov (DevExpress) <litvinov@corp.devexpress.com>
2016-04-15 08:02:03 +0300

    run tests

--- nunit2xunitproxy/nunit2xunitproxy/Program.cs
+++ nunit2xunitproxy/nunit2xunitproxy/Program.cs
@@ -1,14 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
-using System.Threading.Tasks;
 using CommandLine;
+using Ionic.Zip;
 using nunit2xunitproxy.nunitTester;
 using NLog;
+using NUnit.Util;
 
 namespace nunit2xunitproxy {
     class Program {
+        private static string xunitfiles = "nunit2xunitproxy.tools.zip";
         static NLog.ILogger log = LogManager.GetCurrentClassLogger();
         static void Main(string[] args) {
             var result = Parser.Default.ParseArguments<CommandLineOptions>(args);
@@ -25,7 +29,24 @@ namespace nunit2xunitproxy {
                 return 1;
             }
 
-            return 0;
+            ExtractXUnut();
+
+            ProjectConfig config = nunitTestProject.ActiveConfig;
+            return ExecuteTests(config.Assemblies);
+        }
+        private static int ExecuteTests(AssemblyList assemblies) {
+            StringBuilder accumulator = new StringBuilder();
+            var assembliesParameter = assemblies.Cast<string>().Aggregate(accumulator, (builder, s) => builder.Append($"{s} ")).ToString();
+            var process = Process.Start("xunit.console.exe", $@"{assembliesParameter} -nunit TestResult.xml -nologo -noappdomain -paralleloption none -noshadow");
+            process.WaitForExit();
+            return process.ExitCode;
+        }
+        static void ExtractXUnut() {
+            var assembly = Assembly.GetExecutingAssembly();
+            using (Stream stream = assembly.GetManifestResourceStream(xunitfiles)) {
+                var file = ZipFile.Read(stream);
+                file.ExtractAll(Path.GetDirectoryName(assembly.Location), ExtractExistingFileAction.OverwriteSilently);
+            }
         }
     }
 }
