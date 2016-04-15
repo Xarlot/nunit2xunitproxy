f3efc4812fd8cc68c87cddcc4a153e252760eecb
Maxim Litvinov (DevExpress) <litvinov@corp.devexpress.com>
2016-04-15 07:10:39 +0300

    initial

---
+++ nunit2xunitproxy/nunit2xunitproxy/CommandLineOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace nunit2xunitproxy {
+    public class CommandLineOptions {
+        [Option('p', "project", HelpText = "NUnit test project or dll", Required = true)]
+        public string Project { get; set; }
+    }
+}
---
+++ nunit2xunitproxy/nunit2xunitproxy/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+using nunit2xunitproxy.nunitTester;
+using NLog;
+
+namespace nunit2xunitproxy {
+    class Program {
+        static NLog.ILogger log = LogManager.GetCurrentClassLogger();
+        static void Main(string[] args) {
+            var result = Parser.Default.ParseArguments<CommandLineOptions>(args);
+            var exitCode = result.MapResult(clo => {
+                return DoWork(clo);
+            },
+            errors => 1);
+            Environment.Exit(exitCode);
+        }
+        static int DoWork(CommandLineOptions clo) {
+            var nunitTestProject = NunitHelper.LoadProject(clo.Project);
+            if (nunitTestProject == null || nunitTestProject.Configs.Count == 0) {
+                log.Error("Project not found or empty");
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+}
